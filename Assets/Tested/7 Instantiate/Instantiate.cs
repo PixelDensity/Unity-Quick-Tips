fd5fe267ea0a8955c0408a924a958121b97f297d
Kevin Malmsten <outofspacegamedevelopment@gmail.com>
2015-06-26 12:53:46 -0500

    UT Instantiate

---
+++ Assets/Tested/7 Instantiate/Instantiate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiate : MonoBehaviour {
+
+	public GameObject projectile;
+
+	void Start () {
+	
+	}
+	
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Instantiate(projectile, transform.position, transform.rotation);
+		}
+	}
+}
